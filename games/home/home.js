cc55dc7879ca247ef98f7b455324ea20369d1b76
MobiusQuine888 <mobiusquine@gmail.com>
2026-03-28 14:46:05 -0600

    Create home.js

---
+++ games/home/home.js
@@ -0,0 +1,9 @@
+// XP BAR DEMO ANIMATION (UI only for now)
+window.addEventListener("load", () => {
+    const fill = document.getElementById("tanuki-xp-fill");
+
+    // TEMP: animate to 40% so you can SEE it working
+    setTimeout(() => {
+        fill.style.width = "40%";
+    }, 300);
+});
