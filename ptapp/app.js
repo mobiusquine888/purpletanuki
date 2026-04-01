87fbc468643e8791de4d95d2547a07cba0799ba3
MobiusQuine888 <mobiusquine@gmail.com>
2026-03-31 19:18:44 -0600

    Create app.js

---
+++ ptapp/app.js
@@ -0,0 +1,6 @@
+// PTapp Root Wrapper (REFORGED)
+// This file exists only for compatibility and Android WebView initialization.
+
+window.onload = function () {
+    console.log("PTapp root loaded. Shell is now active.");
+};
